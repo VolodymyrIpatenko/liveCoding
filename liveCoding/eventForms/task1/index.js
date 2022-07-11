bf03f4c4b2573a1296ed8a21936e47070564a335
VolodymyrIpatenko <81745231+VolodymyrIpatenko@users.noreply.github.com>
2022-07-11 14:42:47 +0300

    Update index.js

--- liveCoding/eventForms/task1/index.js
+++ liveCoding/eventForms/task1/index.js
@@ -43,8 +43,9 @@ const formElem = document.querySelector('.login-form');
 const onFormSubmit = event => {
   event.preventDefault();
   const formData = [...new FormData(formElem)].reduce(
-    (acc, [field, value]) => ({ ...acc, [field]: value })
+    (acc, [field, value]) => ({ ...acc, [field]: value }),
+    {}
   );
-  alert(formData);
+  alert(JSON.stringify(formData));
 };
 formElem.addEventListener('submit', onFormSubmit);
